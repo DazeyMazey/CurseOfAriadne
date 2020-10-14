5e8d0ade13423417f9be4b9324748ef2733bfc69
Aleana Keiko Perry <31264839+DazeyMazey@users.noreply.github.com>
2020-10-13 18:43:59 -0600

    Day 2
    Added icon assets and have all icons open a window. Note from friend is now implemented

--- Curse of Ariadne/Assets/Scripts/GameTextBehavior.cs
+++ Curse of Ariadne/Assets/Scripts/GameTextBehavior.cs
@@ -24,6 +24,7 @@ public class GameTextBehavior : MonoBehaviour
             // add send command to game later
             textbars.text += "\n> " + field.text;
             field.text = "";
+            field.ActivateInputField();
         }
     }
 }
--- Curse of Ariadne/Assets/Scripts/SpookyEvents.cs
+++ Curse of Ariadne/Assets/Scripts/SpookyEvents.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// This controls all the spooky non text game events and triggers the death system on 0% sanity
 public class SpookyEvents : MonoBehaviour
 {
-    public int sanity;
+    public int sanity = 100;
     public Text SanityHolder;
 
+    public float SPOOK_MIN_TIME;
+    public float SPOOK_MAX_TIME;
 
+    private float curr_time;
+    private float rand_time;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,6 @@ public class SpookyEvents : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SanityHolder.text = sanity.ToString() + "%";
     }
 }
